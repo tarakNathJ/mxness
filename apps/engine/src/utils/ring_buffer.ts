61bb1824882ac1e3ff7a5af946cb7b063cbb647f
TarakNathJana <janatarak739@gmail.com>
2025-12-09 17:04:57 +0530

    fix monitoring packges

--- apps/engine/src/utils/ring_buffer.ts
+++ apps/engine/src/utils/ring_buffer.ts
@@ -1,8 +1,8 @@
 class ring_buffer {
     private size: number =0;
     private buffer: any[];
-    private writeIndex: number = 0;
-    private readIndex: number = 0;;
+    private write_index: number = 0;
+    private read_index: number = 0;;
 
     constructor(size: number) {
       this.size = size;
@@ -11,14 +11,14 @@ class ring_buffer {
 
     
     public ring_publish(value: any) {
-      this.buffer[this.writeIndex] = value;
-      this.writeIndex = (this.writeIndex + 1) % this.size;
+      this.buffer[this.write_index] = value;
+      this.write_index = (this.write_index + 1) % this.size;
     }
 
     public ring_consumer (){
-        if( this.readIndex === this.writeIndex) return null;
-        const value = this.buffer[this.readIndex];
-        this.readIndex = (this.readIndex + 1) % this.size;
+        if( this.read_index === this.write_index) return null;
+        const value = this.buffer[this.read_index];
+        this.read_index = (this.read_index + 1) % this.size;
         return value;
     }
 }
--- packages/monitoring/src/index.ts
+++ packages/monitoring/src/index.ts
@@ -1,79 +1,135 @@
-import type { Request, Response, NextFunction } from "express";
-
 import promClient from "prom-client";
-
 import { config } from "dotenv";
 config();
 
-// metrix collect
+class PrometheusService {
+  public registry: promClient.Registry;
 
-const req_res_time = new promClient.Histogram({
-  name:"http_express_req_res_time",
-  help:"this tells how much time is taken by rreq and res",
-  labelNames: ["method","route","status_code"],
-  buckets: [ 1 ,30,50, 120, 200 ,400, 500, 800 ,1000, 2000]
-});
+  // metrix
+  public req_res_time: promClient.Histogram<string>;
+  public ws_connections: promClient.Counter<string>;
+  public ws_messages_received: promClient.Counter<string>;
+  public ws_messages_sent: promClient.Counter<string>;
+  public ws_active_connections: promClient.Gauge<string>;
+
+  public trade_processing_duration: promClient.Histogram<string>;
+  public kafka_messages_consumed: promClient.Counter<string>;
+  public kafka_messages_produced: promClient.Counter<string>;
+  public trade_tp_triggered: promClient.Counter<string>;
+  public trade_sl_triggered: promClient.Counter<string>;
+  public trade_hold: promClient.Counter<string>;
+
+  constructor() {
+    this.registry = new promClient.Registry();
+
+    // Collect default metrics
+    promClient.collectDefaultMetrics({ register: this.registry });
+
+    //  HTTP Histogram
+    this.req_res_time = new promClient.Histogram({
+      name: "http_express_req_res_time",
+      help: "HTTP request/response time",
+      labelNames: ["method", "route", "status_code"],
+      buckets: [1, 30, 50, 120, 200, 400, 500, 800, 1000, 2000],
+    });
+
+    // WebSocket Metrics
+    this.ws_connections = new promClient.Counter({
+      name: "ws_connections_total",
+      help: "Total number of WebSocket connections",
+    });
+
+    this.ws_messages_received = new promClient.Counter({
+      name: "ws_messages_received_total",
+      help: "Total number of WebSocket messages received",
+    });
+
+    this.ws_messages_sent = new promClient.Counter({
+      name: "ws_messages_sent_total",
+      help: "Total number of WebSocket messages sent",
+    });
+
+    this.ws_active_connections = new promClient.Gauge({
+      name: "ws_active_connections",
+      help: "Number of active WebSocket connections",
+    });
 
 
-
-const server_register = new promClient.Registry();
-const collectDefaultMetrics = promClient.collectDefaultMetrics;
-collectDefaultMetrics({ register: promClient.register });
-
-const metrix_handler = async (
-  req: Request,
-  res: Response,
-  next: NextFunction
-) => {
-  res.set("Content-Type", promClient.register.contentType);
-  return res.send(await promClient.register.metrics());
-};
-
-function create_job_metrics(job_name: string) {
-  if (!/^[a-zA-Z_:][a-zA-Z0-9_:]*$/.test(job_name)) {
-    throw new Error(
-      `Invalid job_name "${job_name}". Metric names must match /^[a-zA-Z_:][a-zA-Z0-9_:]*$/`
-    );
+    // kafka
+    this.trade_processing_duration = new promClient.Histogram({
+      name: "trade_processing_duration_seconds",
+      help: "Time taken to process a single trade event",
+      buckets: [0.005, 0.01, 0.05, 0.1, 0.2, 0.5, 1],
+    });
+    this.kafka_messages_consumed = new promClient.Counter({
+      name: "kafka_messages_consumed_total",
+      help: "Total Kafka messages consumed",
+    });
+    this.kafka_messages_produced = new promClient.Counter({
+      name: "kafka_messages_produced_total",
+      help: "Total Kafka messages produced",
+    });
+    this.trade_tp_triggered = new promClient.Counter({
+      name: "trade_take_profit_triggered_total",
+      help: "Total number of TP triggers",
+    });
+    this.trade_sl_triggered = new promClient.Counter({
+      name: "trade_stop_loss_triggered_total",
+      help: "Total number of SL triggers",
+    });
+    this.trade_hold = new promClient.Counter({
+      name: "trade_hold_total",
+      help: "Trades kept on hold",
+    });
+    this.registry.registerMetric(this.req_res_time);
+    this.registry.registerMetric(this.ws_connections);
+    this.registry.registerMetric(this.ws_messages_received);
+    this.registry.registerMetric(this.ws_messages_sent);
+    this.registry.registerMetric(this.ws_active_connections);
+    this.registry.registerMetric(this.trade_processing_duration);
+    this.registry.registerMetric(this.kafka_messages_consumed);
+    this.registry.registerMetric(this.kafka_messages_produced);
+    this.registry.registerMetric(this.trade_tp_triggered);
+    this.registry.registerMetric(this.trade_sl_triggered);
+    this.registry.registerMetric(this.trade_hold);
   }
 
-  const register = new promClient.Registry();
+  public create_job_metrics(job_name: string) {
+    if (!/^[a-zA-Z_:][a-zA-Z0-9_:]*$/.test(job_name)) {
+      throw new Error(`Invalid job_name "${job_name}"`);
+    }
+    const job_registry = new promClient.Registry();
+    const gateway = new promClient.Pushgateway(
+      process.env.CLIENT_URL!,
+      {},
+      job_registry
+    );
 
-  const gateway = new promClient.Pushgateway(
-    process.env.CLIENT_URL!,
-    {},
-    register
-  );
+    const job_counter = new promClient.Counter({
+      name: `${job_name}_runs_total`,
+      help: `Total number of runs for job ${job_name}`,
+      registers: [job_registry],
+    });
 
-  const job_counter = new promClient.Counter({
-    name: `${job_name}_runs_total`,
-    help: `total number of runs fro job ${job_name}`,
-    registers: [register],
-  });
-  const job_duration = new promClient.Histogram({
-    name: `${job_name}_duration_seconds`,
-    help: `duration of job ${job_name}`,
-    registers: [register],
-    buckets: [0.1, 0.5, 1, 1.5, 2, 5, 10, 30],
-  });
+    const job_duration = new promClient.Histogram({
+      name: `${job_name}_duration_seconds`,
+      help: `Duration of job ${job_name}`,
+      registers: [job_registry],
+      buckets: [0.1, 0.5, 1, 1.5, 2, 5, 10, 30],
+    });
 
-  return {
-    job_counter,
-    job_duration,
-    push: async () => {
-      try {
-        await gateway.pushAdd({ jobName: job_name });
-        console.log(` Metrics pushed for job: ${job_name}`);
-      } catch (err: any) {
-        console.error("Failed to push metrics:", err.message);
-      }
-    },
-  };
+    return {
+      job_counter,
+      job_duration,
+      push: async () => {
+        try {
+          await gateway.pushAdd({ jobName: job_name });
+        } catch (error: any) {
+          console.error("Failed to push metrics:", error.message);
+        }
+      },
+    };
+  }
 }
 
-export {
-
-  metrix_handler,
-  create_job_metrics,
-  server_register,
-  req_res_time
-};
+export default PrometheusService;
